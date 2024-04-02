1b94cd867052253d26ba6ed411e1a9a554e9c3b0
valentin <valentin@zalter.com>
2024-04-02 13:49:50 +0300

    Ad-hoc feature accessible with  string literal marker

--- src/index.mjs
+++ src/index.mjs
@@ -5,6 +5,7 @@ export * from "./observed-object.mjs";
 export * from "./templating-engines/html/observed-template.mjs";
 export {observedTemplate as html} from "./templating-engines/html/observed-template.mjs";
 export {observedTrimmedTemplate as htmlT} from "./templating-engines/html/observed-template.mjs";
+export {observedTemplateAdhoc as adHoc} from "./templating-engines/html/observed-template.mjs";
 export * from "./templating-engines/css/observed-css.mjs";
 export {buildObservedStyle as styled} from "./templating-engines/css/observed-css.mjs";
 export * from "./templating-engines/html/conditional.mjs";
